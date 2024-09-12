28805841cfa0547ba3d779ac65508d453a124bd8
sumantKr <sumantkumar17200@gmail.com>
2024-09-12 23:46:52 +0530

    svelte init

---
+++ src/client/src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1 class="text-2xl">HELLO</h1>  
+</main>
+
+<style>
+
+</style>
--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
 // https://vitejs.dev/config/
-export default ({ mode }:any) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  console.debug("🚀 ~ process.env :", process.env.VITE_API_URL )
-  
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api/v1": process.env.VITE_API_URL as string,
-        changeOrigin:"true"
-      }
-    }
-  })
-}
+export default defineConfig({
+  plugins: [svelte()],
+})
